5bca91d54781fa07fc60866e61c40b34aaea5420
Hasan Sulaeman <hasanregius@gmail.com>
2019-07-03 15:16:42 -0700

    Reviewed GLM Code

--- Statistics/GLM.R
+++ Statistics/GLM.R
@@ -1,20 +1,22 @@
 ############################################################
 # Binary Logistic Regression for Sierra Nevada Historical Bd
+# Hasan Sulaeman
 ############################################################
 
 # Library and data setup ----
 library(Hmisc)
+library(pscl)
 setwd("/Users/hasansulaeman/Dropbox/Sam's Sierra Bd work/Data")
 mue = read.csv('samsara.csv')
 
-# Finding highly correlated variables
+# Finding highly correlated variables ----
 factors = mue[,c(6,29:41)]
 factor.cor = rcorr(as.matrix(factors))
 ## NOTE: tmean is highly correlated with tmin, tmax, and elevation
-## Scaling the numeric data so that they can be more evenly compared
+## Scaling the Variables ----
 mue[,c(29:36,38,40:41)] = scale(mue[,c(29:36,38,40:41)])
 
-# The tmin, tmean, and tmax are highly correlated with one another
+# The tmin, tmean, and tmax are highly correlated with one another, excluded
 lr = glm(BdStatus ~ distwater + factor(catwater) + Roads + ppt
      + tmean + Popdensity2010 + croplands2005 + Built2009 + Species +
      Popdensity2010 + Railways + Pasture2009 + HFP2009, data=mue)
@@ -28,8 +30,8 @@ sink(file="All_Years_GLM_Results.txt")
 summary(winner) 
 sink()
 
-# Pre-1970 Data and LM
-pre70=subset(mue, Year < 1970)
+# Pre-1970 Data and GLM ----
+pre70 = subset(mue, Year < 1970)
 lr = lm(BdStatus ~ dist.closest.wb + factor(cat.closest.wb) + Roads
         + tmean + elevation + Popdensity2010 + HFP2009, data=pre70)
 ayo = step(lr, direction="both")
@@ -46,27 +48,27 @@ post70=subset(mue, Year >= 1970)
 lr = lm(BdStatus ~ dist.closest.wb + factor(cat.closest.wb) + Roads
         + tmean + Elevation.m. + Popdensity2010 + HFP2009 + FirstPosDist, data=post70)
 ayo = step(lr)
+
+# Post 1970 Data and GLM ----
 post70.lm = lm(BdStatus ~ dist.closest.wb + Roads + Elevation.m. + HFP2009 + FirstPosDist, data=post70)
 summary(post70.lm)
 # Positive relationship = Distance to closest water body, roads (p=0.03, p<0.01; respectively)
 # Negative relationship = NA
-sink(file="Post70_LM_Results.txt")
+sink(file = "Post70_LM_Results.txt")
 summary(post70.lm) 
 sink()
 
-# Let's try out zeroinflated model, which might be better for us
-library(pscl)
-
-# Running the same model but zeroinflated
+# Zero-Inflated Model for Comparison ----
+## Running the same model but zeroinflated
 zeroinf = zeroinfl(BdStatus ~ Roads + tmean + HFP2009 + FirstPosDist, data=mue, dist = "negbin", link= "logit")
 summary(zeroinf) 
 
-# Let's save the result
+## Let's save the result
 sink(file="zeroinflated_LM_Results.txt")
 summary(zeroinf) 
 sink()
 
-# Can't run GLM on the Zscore with too many zeros 
+## Can't run GLM on the Zscore with too many zeros 
 zscore = subset(mue, ZEScore > 0)
 hist(zscore$ZEScore)
 
